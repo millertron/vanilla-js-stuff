04263ed1db4ef11ec631dca025986a39b53db15c
millertron <senjinkoha@gmail.com>
2022-02-26 18:51:35 +0000

    feat: :sparkles: - added additional validations

--- form-validator/script.js
+++ form-validator/script.js
@@ -30,13 +30,37 @@ const checkRequired = (inputs) => {
     })
 }
 
-const isValidEmail = (value) => {
-    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(String(value).toLowerCase())
+const checkLength = (input, minLength, maxLength) => {
+    if (input.value.length < minLength) {
+        showError(input, `${getFieldLabel(input)} must be at least ${minLength} characters`)
+    } else if (input.value.length > maxLength) {
+        showError(input, `${getFieldLabel(input)} must be no longer than ${maxLength} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+const checkEmail = (input) => {
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(String(input.value).toLowerCase())) {
+        showError(input, 'Not a valid email')
+    } else {
+        showSuccess(input)
+    }
+}
+
+const checkPasswordsMatch = () => {
+    if (passwordInput.value !== passwordConfirmInput.value) {
+        showError(passwordConfirmInput, 'Password does not match')
+    }
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkRequired([usernameInput, emailInput, passwordInput, passwordConfirmInput])
+    checkLength(username, 3, 15)
+    checkLength(password, 9, 255)
+    checkEmail(emailInput)
+    checkPasswordsMatch()
 })
 
