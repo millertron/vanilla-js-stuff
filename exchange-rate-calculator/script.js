0a52ef3196cc82f0dcfb4db8c2c262ba074088ae
millertron <senjinkoha@gmail.com>
2022-03-01 16:08:02 +0000

    feat: sparkles: - implement calculator

---
+++ exchange-rate-calculator/script.js
@@ -0,0 +1,37 @@
+const currencyOne = document.getElementById('currencyOne')
+const amountOne = document.getElementById('amountOne')
+
+const currencyTwo = document.getElementById('currencyTwo')
+const amountTwo = document.getElementById('amountTwo')
+
+const rate = document.getElementById('rate')
+const swapButton = document.getElementById('swap')
+
+const calculate = () => {
+    const currencyOneVal = currencyOne.value
+    const amountOneVal = amountOne.value
+
+    const currencyTwoVal = currencyTwo.value
+    
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOneVal}`)
+        .then(response => response.json())
+        .then(data => {
+            const rateVal = data.rates[currencyTwoVal]
+            rate.innerText = `1 ${currencyOneVal} = ${rateVal} ${currencyTwoVal}`
+            amountTwo.value = (amountOneVal * rateVal).toFixed(2)
+        })
+
+}
+
+currencyOne.addEventListener('change', calculate)
+amountOne.addEventListener('input', calculate)
+currencyTwo.addEventListener('change', calculate)
+amountTwo.addEventListener('input', calculate)
+swapButton.addEventListener('click', () => {
+    const tmp = currencyOne.value
+    currencyOne.value = currencyTwo.value
+    currencyTwo.value = tmp
+    calculate()
+})
+
+calculate();
