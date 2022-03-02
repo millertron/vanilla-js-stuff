59598afd71a1f153deacdbe3cc90945e43115b5d
millertron <senjinkoha@gmail.com>
2022-03-02 21:08:57 +0000

    feat: :sparkles: - implement key input and game

--- hangman/script.js
+++ hangman/script.js
@@ -1,9 +1,10 @@
 const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrongLetters')
 const playButton = document.getElementById('playButton')
-const modal = document.getElementById('modal')
-const notification = document.getElementById('notification')
+const modal = document.getElementById('modalContainer')
+const notificationContainer = document.getElementById('notificationContainer')
 const finalMessage = document.getElementById('finalMessage')
+let endgame = false
 
 const figureBodyParts = document.querySelectorAll('.figure-bodypart')
 
@@ -23,8 +24,69 @@ const displayWord = () => {
 
     const innerWord = wordEl.innerText.replace(/\n/g, '')
     if (innerWord === selectedWord) {
+        finalMessage.innerText = 'Congratulations! You won!'
+        modal.style.display = 'flex'
+        endgame = true
+    }
+}
+
+const showNotification = () => {
+    notificationContainer.classList.add('show')
+    setTimeout(() => {
+        notificationContainer.classList.remove('show')
+    }, 2000)
+}
+
+const updateWrong = () => {
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`).join(', ')}
+    `
+
+    const errors = wrongLetters.length
+    figureBodyParts.forEach((part, index) => {    
+        if (index < errors) {
+            part.style.display = 'block'
+        } else {
+            part.style.display = 'none'
+        }
+    })
+
+    if (errors === figureBodyParts.length) {
+        endgame = true
+        finalMessage.innerText = 'You lost. Better luck next time...'
         modal.style.display = 'flex'
     }
 }
+
+window.addEventListener('keyup', e => {
+    const pressedKey = e.key
+    if (!endgame && pressedKey.length === 1 && pressedKey.match(/[a-zA-Z]/)) {
+        if (selectedWord.includes(pressedKey)) {
+            if (!correctLetters.includes(pressedKey)) {
+                correctLetters.push(pressedKey)
+                displayWord()
+            } else {
+                showNotification()
+            }
+        } else {
+            if (!wrongLetters.includes(pressedKey)) {
+                wrongLetters.push(pressedKey)
+                updateWrong()
+            }
+        }
+    }
+})
+
+playButton.addEventListener('click', () => {
+    correctLetters.splice(0)
+    wrongLetters.splice(0)
+
+    selectedWord = generateWord()
+    endgame = false
+    displayWord()
+    updateWrong()
+    modal.style.display = 'none'
+})
 
 displayWord();
