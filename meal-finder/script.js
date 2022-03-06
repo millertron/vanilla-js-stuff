defcb8b94b9bbad704ee5a6f1d60046a80ae56e3
millertron <senjinkoha@gmail.com>
2022-03-06 17:38:23 +0000

    feat: :sparkles: - implemented single meal fetch

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -34,4 +34,51 @@ const searchMeal = (e) => {
     }
 }
 
+const addMealToDOM = (meal) => {
+    const ingredients = []
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`]
+        if(ingredient) {
+            ingredients.push(`${ingredient} - ${meal[`strMeasure${i}`]}`)
+        } else {
+            break
+        }
+    }
+    singleMealEl.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+            <div class="single-meal-info">
+                ${meal.strCategory ? `<p>Category: ${meal.strCategory}</p>` : ''}
+                ${meal.strArea ? `<p>Area: ${meal.strArea}</p>` : ''}
+            </div>
+            <div class="main">
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+                </ul>
+                <h2>Instructions</h2>
+                <p>${meal.strInstructions}</p>
+            </div>
+        </div>
+    `
+}
+
+const getMealById = id => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0]
+            addMealToDOM(meal)
+        })
+}
+
 submit.addEventListener('submit', searchMeal)
+
+mealsEl.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => item.classList ? item.classList.contains('meal-info') : false)
+    if (mealInfo) {
+        const mealId = mealInfo.getAttribute('data-mealId')
+        getMealById(mealId)
+    }
+})
