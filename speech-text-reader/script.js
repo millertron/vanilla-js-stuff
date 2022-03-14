e65230d6d42e580e7b0d22f0795a7a5bba277dbb
millertron <senjinkoha@gmail.com>
2022-03-14 12:53:37 +0000

    feat: :sparkles: - added image boxes

---
+++ speech-text-reader/script.js
@@ -0,0 +1,70 @@
+const main = document.querySelector('main')
+const voiceSelect = document.getElementById('voices')
+const textArea = document.getElementById('text')
+const readButton = document.getElementById('read')
+const toggleButton = document.getElementById('toggle')
+const closeButton = document.getElementById('close')
+
+const data = [
+    {
+        image: `./img/drink.jpg`,
+        text: "I'm thirsty"
+    },
+    {
+        image: `./img/food.jpg`,
+        text: "I'm hungry"
+    },
+    {
+        image: `./img/tired.jpg`,
+        text: "I'm tired"
+    },
+    {
+        image: `./img/hurt.jpg`,
+        text: "I'm hurt"
+    },
+    {
+        image: `./img/happy.jpg`,
+        text: "I'm happy"
+    },
+    {
+        image: `./img/angry.jpg`,
+        text: "I'm angry"
+    },
+    {
+        image: `./img/sad.jpg`,
+        text: "I'm sad"
+    },
+    {
+        image: `./img/scared.jpg`,
+        text: "I'm scared"
+    },
+    {
+        image: `./img/outside.jpg`,
+        text: "I want to go outside"
+    },
+    {
+        image: `./img/home.jpg`,
+        text: "I want to go home"
+    },
+    {
+        image: './img/school',
+        text: 'I want to go to school'
+    },
+    {
+        image: `./img/grandma`,
+        text: "I want to go to grandma's"
+    }
+]
+
+const createBox = (item) => {
+    const box = document.createElement('div')
+    const { image, text } = item
+    box.classList.add('box')
+    box.innerHTML = `
+        <img src="${image}" alt="${text}" />
+        <p class="info">${text}</p>
+    `
+    main.appendChild(box)
+}
+
+data.forEach(createBox)
