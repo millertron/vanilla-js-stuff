7683f62d910640ccbd8dfe734dc6f31eb947bbe1
millertron <senjinkoha@gmail.com>
2022-03-18 22:42:05 +0000

    feat: :sparkles: - draw paddle

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -5,6 +5,8 @@ const rulesSection = document.getElementById('rules')
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+const canvasItemColor = '#0095dd'
+
 const ball = {
     x: canvas.width / 2,
     y: canvas.height / 2,
@@ -14,15 +16,37 @@ const ball = {
     dy: -4
 }
 
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    width: 80,
+    height: 10,
+    speed: 8,
+    dx: 0
+}
+
 const drawBall = () => {
     ctx.beginPath()
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
-    ctx.fillStyle = '#0095dd'
+    ctx.fillStyle = canvasItemColor
     ctx.fill()
     ctx.closePath()
+}
+
+const drawPaddle = () => {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.width, paddle.height)
+    ctx.fillStyle = canvasItemColor
+    ctx.fill()
+    ctx.closePath()
+}
+
+const draw = () => {
+    drawBall()
+    drawPaddle()
 }
 
 rulesButton.addEventListener('click', () => rules.classList.add('show'))
 closeButton.addEventListener('click', () => rules.classList.remove('show'))
 
-drawBall()
+draw()
