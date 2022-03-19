c957f2e3fc1a2f659800dfb62bd85b888465fbd4
millertron <senjinkoha@gmail.com>
2022-03-19 20:02:20 +0000

    feat: :sparkles: - implement drag and swap

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -16,7 +16,62 @@ const itemsData = [
 
 const listItems = []
 
-let dragStartIndex;
+let dragStartIndex
+let currentDragItem
+
+const swapItems = (endIndex) => {
+    const source = listItems[dragStartIndex].querySelector('.draggable')
+    const target = listItems[endIndex].querySelector('.draggable')
+
+    listItems[dragStartIndex].appendChild(target)
+    listItems[endIndex].appendChild(source)
+
+    source.closest('li').classList.remove('over')
+}
+
+const dragStart = (e) => {
+    const item = e.target
+    dragStartIndex = +item.closest('li').getAttribute('data-index')
+}
+
+const dragOver = (e) => {
+    e.preventDefault()
+}
+
+const dragDrop = (e) => {
+    const item = e.target
+    const dragEndIndex = +item.closest('li').getAttribute('data-index')
+    swapItems(dragEndIndex)
+}
+
+const dragEnter = (e) => {
+    const item = e.target
+    currentDragItem = item.closest('li')
+    item.closest('li').classList.add('over')
+}
+
+const dragLeave = (e) => {
+    const item = e.target
+    if (currentDragItem !== item.closest('li')) {
+        item.closest('li').classList.remove('over')
+    }
+}
+
+const addDragEventListeners = () => {
+    const draggables = document.querySelectorAll('.draggable')
+    const dragListItems = document.querySelectorAll('.draggable-list li')
+
+    draggables.forEach(item => {
+        item.addEventListener('dragstart', dragStart)
+    })
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver)
+        item.addEventListener('drop', dragDrop)
+        item.addEventListener('dragdrop', dragDrop)
+        item.addEventListener('dragenter', dragEnter)
+        item.addEventListener('dragleave', dragLeave)
+    })
+}
 
 const createList = () => {
     [...itemsData]
@@ -27,7 +82,7 @@ const createList = () => {
             listItem.setAttribute('data-index', index)
             listItem.innerHTML = `
                 <span class="number">${index + 1}</span>
-                <div class="draggable" dtaggable="true">
+                <div class="draggable" draggable="true">
                     <p class="person-name">${item.value}</p>
                     <i class="fas fa-grip-lines"></i>
                 </div>
@@ -35,6 +90,7 @@ const createList = () => {
             listItems.push(listItem)
             draggableList.appendChild(listItem)
         })
+    addDragEventListeners()
 }
 
 createList()
