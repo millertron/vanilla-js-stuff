7c8e1adbea54b83cfed19efbf72f9b010fda3e56
millertron <senjinkoha@gmail.com>
2022-03-19 17:23:09 +0000

    feat: :sparkles: - randomize list items

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -19,19 +19,22 @@ const listItems = []
 let dragStartIndex;
 
 const createList = () => {
-    [...itemsData].forEach((person, index) => {
-        const listItem = document.createElement('li')
-        listItem.setAttribute('data-index', index)
-        listItem.innerHTML = `
-            <span class="number">${index + 1}</span>
-            <div class="draggable" dtaggable="true">
-                <p class="person-name">${person}</p>
-                <i class="fas fa-grip-lanes"></i>
-            </div>
-        `
-        listItems.push(listItem)
-        draggableList.appendChild(listItem)
-    })
+    [...itemsData]
+        .map(a => ({ value: a, sort: Math.random() }))
+        .sort((i, j) => i.sort - j.sort)
+        .forEach((item, index) => {
+            const listItem = document.createElement('li')
+            listItem.setAttribute('data-index', index)
+            listItem.innerHTML = `
+                <span class="number">${index + 1}</span>
+                <div class="draggable" dtaggable="true">
+                    <p class="person-name">${item.value}</p>
+                    <i class="fas fa-grip-lanes"></i>
+                </div>
+            `
+            listItems.push(listItem)
+            draggableList.appendChild(listItem)
+        })
 }
 
 createList()
